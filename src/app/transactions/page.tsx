0bb444989eaab4b48588416e23ae929ebe3363a8
Jasmin Hidanovic <dexinho008@gmail.com>
2024-04-23 14:03:34 +0200

    added bgColor

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -3,12 +3,14 @@ import { Transaction } from "../../../types/types";
 import createTableRows from "@/utility/createTableRows";
 import Table from "@/components/Table";
 import NewTransaction from "@/components/NewTransaction";
+import { makeDateEuropean } from "@/utility/makeDateEuropean";
 
 const headers = ["id", "order id", "date", "from state", "to state"];
 
 const TransactionsPage = async () => {
   const transactions = await getTransactions();
-  const rows = createTableRows<Transaction>(transactions);
+  const europeanTransactionsDates = makeDateEuropean<Transaction>(transactions);
+  const rows = createTableRows<Transaction>(europeanTransactionsDates);
 
   return (
     <div className="flex flex-col gap-4">
