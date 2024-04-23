a6cc588450e9d86115af8fb2cba047923870795d
Jasmin Hidanovic <dexinho008@gmail.com>
2024-04-23 14:03:57 +0200

    added bgColor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Form from "@/components/Form";
 const HomePage = () => {
   const orderTitle = `Order details`;
 
-  return <Form title={orderTitle} />;
+  return <Form title={orderTitle} bgColor={"bg-slate-200"} />;
 };
 
 export default HomePage;
