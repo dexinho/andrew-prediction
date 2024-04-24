878b298a330a63717f706b1107c892e4de49aa13
Jasmin Hidanovic <dexinho008@gmail.com>
2024-04-24 10:12:08 +0200

    added -top-16

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import Form from "@/components/Form";
 const HomePage = () => {
   const orderTitle = `Order details`;
 
-  return <Form title={orderTitle} bgColor={"bg-slate-200"} />;
+  return (
+    <div className="relative -top-16">
+      <Form title={orderTitle} bgColor={"bg-slate-200"} />
+    </div>
+  );
 };
 
 export default HomePage;
